c008bd731e6ec0b96504f5441a4622f9095948f7
pauloalvesm <paulo.alves7351@gmail.com>
2026-06-18 20:25:52 -0300

    feat: apply the heartbeat example

---
+++ src/StudiesJobs.App/01-Basic/HeartbeatJob.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace StudiesJobs.App._01_Basic;
+
+public class HeartbeatJob : BackgroundService
+{
+    private readonly ILogger<HeartbeatJob> _logger;
+    private readonly TimeSpan _period = TimeSpan.FromSeconds(5);
+
+    public HeartbeatJob(ILogger<HeartbeatJob> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Heartbeat Job started.");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                _logger.LogInformation("Checking services status at: {Time}", DateTime.Now.ToString("HH:mm:ss"));
+                await ExecuteAvailabilityCheckAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while executing the Heartbeat Job.");
+            }
+
+            await Task.Delay(_period, stoppingToken);
+        }
+
+        _logger.LogInformation("Heartbeat Job finished.");
+    }
+
+    private Task ExecuteAvailabilityCheckAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
