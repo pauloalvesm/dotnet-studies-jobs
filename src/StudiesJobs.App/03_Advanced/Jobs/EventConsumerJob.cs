e942570bec5769e957d1b9db3947c972efd509db
pauloalvesm <paulo.alves7351@gmail.com>
2026-06-21 13:27:45 -0300

    feat: created jobs for producer and consumer examples

---
+++ src/StudiesJobs.App/03_Advanced/Jobs/EventConsumerJob.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using StudiesJobs.App._03_Advanced.Queues;
+
+namespace StudiesJobs.App._03_Advanced.Jobs;
+
+public class EventConsumerJob : BackgroundService
+{
+    private readonly InMemoryIntegrationQueue _queue;
+    private readonly ILogger<EventConsumerJob> _logger;
+
+    public EventConsumerJob(InMemoryIntegrationQueue queue, ILogger<EventConsumerJob> logger)
+    {
+        _queue = queue;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Consumer job started and waiting for items...");
+
+        await foreach (var item in _queue.ReadFromQueueAsync(stoppingToken))
+        {
+            _logger.LogInformation("<= CONSUMER: Processing event ID: {Id} | Content: {Payload}", item.Id, item.Payload);
+            await Task.Delay(500, stoppingToken);
+        }
+    }
+}
---
+++ src/StudiesJobs.App/03_Advanced/Queues/InMemoryIntegrationQueue.cs
@@ -0,0 +1,18 @@
+﻿using StudiesJobs.App._03_Advanced.Models;
+using System.Threading.Channels;
+
+namespace StudiesJobs.App._03_Advanced.Queues;
+
+public class InMemoryIntegrationQueue
+{
+    private readonly Channel<IntegrationItem> _queue;
+
+    public InMemoryIntegrationQueue()
+    {
+        var options = new BoundedChannelOptions(100) { FullMode = BoundedChannelFullMode.Wait };
+        _queue = Channel.CreateBounded<IntegrationItem>(options);
+    }
+
+    public async ValueTask WriteToQueueAsync(IntegrationItem item) => await _queue.Writer.WriteAsync(item);
+    public IAsyncEnumerable<IntegrationItem> ReadFromQueueAsync(CancellationToken cancellationToken) => _queue.Reader.ReadAllAsync(cancellationToken);
+}
